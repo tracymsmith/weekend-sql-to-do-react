3a201a44103b91b485118d61d5b0f6cd1709fe2f
Tracy Smith <tmhsllc@icloud.com>
2024-08-06 00:18:13 -0500

    created sql file and db and added to db; tested db in Portico; created GET route in todo.router.js file; tested in Postman however keep getting 404 error code

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,10 @@
 const pg = require('pg');
-
 const pool = new pg.Pool({
+    database: "weekend-to-do-app", 
     host: 'localhost',
     port: 5432,
-    database: 'weekend-to-do-app', 
 });
 
 module.exports = pool;
+
+
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool.js');
+const pool = require('../modules/pool');
 
 // GET
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "tasks";';
+
+    pool.query(queryText)
+        .then((result) => {
+            console.log(result.rows);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making query: ${queryText}`, error);
+            res.sendStatus(500);
+        });
+});
+
 
 // POST
+
 
 // PUT
 
