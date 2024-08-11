3aa1988da4fbd2320b2e89524eeebfffd9d50c88
Tracy Smith <tmhsllc@icloud.com>
2024-08-10 21:01:03 -0500

    added const description set description useState to app.jsx, tested on postgres, postico & postman plus data showed up on DOM but not formatted nor HTML effects

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,18 @@
 import {useState} from 'react';
+import React from 'react';
+import todoApp from './todoApp'
+
+// function App() {
+  // const [shoppingList, setShoppingList] = useState([]);
+  // const [name, setName] = useState('');
+  // const [quantity, setQuantity] = useState('');
+  // const [unit, setUnit] = useState('');
 
 function App () {
+  const [description, setDescription] = useState([])};
   
   return (
+
     <div>
       <h1>TO DO APP</h1>
     </div>
